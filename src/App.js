a6de512595a17cfea2f32f954afe8153d0225e05
Thawatchai Jaihan <jh.thawatchai@gmail.com>
2022-08-20 16:42:58 +0700

    part 7

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "computer";
 function App() {
   return (
     <div>
-     <WordCard value="hello"/>
+
+     <WordCard value="computer"/>
+
+
     </div>
     )
 }
